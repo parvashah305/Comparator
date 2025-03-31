9bec17d317f0032e700eb770c2e8708641148572
parvashah305 <parvashah305@gmail.com>
2025-03-31 23:23:40 +0530

    size limit extended

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors({
     origin:"https://comparator-five.vercel.app",
 }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api", compareRoutes);
 
